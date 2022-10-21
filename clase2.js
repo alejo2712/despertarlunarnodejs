7c1d59808b1b0e4759c83430bf0a7f173ca9e20d
Ale Piedrabuena <alejopiedrabuena@gmail.com>
2022-10-21 02:42:47 -0300

    Entrega de la clase2 lista para entregar

--- clase2.js
+++ clase2.js
@@ -63,13 +63,41 @@ class Usuario {
         this.mascotas = mascotas;
     }
 
-    getFullName (nombre, apellido){
+    getFullName(nombre, apellido) {
         console.log (`EL nombre completo es ${nombre} ${apellido}`)
     }
 
+    addMascota(mascota) {
+        this.mascotas.push(mascota);
+        console.log(`Agregaste la mascota: ${mascota}`);
+        console.log(`Tu array de mascotas quedó así ${this.mascotas}`);
+    }
+
+    countMascotas() {
+        console.log(`La cantidad de mascotas es: ${this.mascotas.length}`);
+    }
+
+    addBook(nombre, autor) {
+        console.log(`Libro nuevo para agregar al array de objetos: ${nombre}, ${autor}`);
+        this.libros.push({nombre: nombre, autor: autor});
+        const books = JSON.stringify(this.libros);
+        console.log(`Ahora tu array de libros quedo asi: ${books}`);
+    }
+
+    getBookNames() {
+        let test = [];
+        for (let i = 0; i < this.libros.length; i++) {
+            test.push(JSON.stringify(this.libros[i].nombre));
+        }
+        console.log(`Los nombres de los libros son: ${test}`);
+    }
 }
 
-const user1 = new Usuario ('user1', 'user1Apellido', {libro1:'libro1', libro2: 'libro2'}, ['mascota1', 'mascota2']);
+const user1 = new Usuario ('user1', 'user1Apellido', [{nombre:'libro1', autor: 'alejo'}], ['mascota1', 'mascota2']);
 
-console.log (user1);
-user1.getFullName(user1.nombre, user1.apellido);
+// console.log (user1);
+// user1.getFullName(user1.nombre, user1.apellido);
+// user1.addMascota('perrito');
+// user1.countMascotas();
+user1.addBook('libro2', 'Rolon');
+user1.getBookNames();
